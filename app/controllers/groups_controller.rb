120a48d9f6a2eeb626238cbbecc05c0ee544da52
Daniel Barbosa Stein <ksatechnology@gmail.com>
2014-03-14 10:49:28 -0300

    Multiplas correcoes e renomeações. Adicao do sistema de tags de inseção - grupos - e campo de comentário para cada item da checklist

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,6 +4,11 @@ class GroupsController < ApplicationController
     @groups = Group.all
   end
   
+  def show
+    @group = Group.find(params[:id])
+    @inspections = Inspection.where('group_id = ' + params[:id]).order("created_at DESC")
+  end
+  
   def new
     @group = Group.new
   end
